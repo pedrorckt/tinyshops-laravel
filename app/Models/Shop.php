83ac1651dd7ff35e1046bbcd3dbd6c91c07f96df
Pedro Emilio Guglielmo <pedro@rckt.com.br>
2023-06-15 21:46:03 -0300

    Controllers, requests, models, policies

---
+++ app/Models/Shop.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'description'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+}
